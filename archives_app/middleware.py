50e2afba1721fc90481dc2feb1b17a116608e2e9
bielrossi15 <bielrossiborba@gmail.com>
2021-10-01 00:39:19 -0300

    Refatorando middleware

--- archives_app/middleware.py
+++ archives_app/middleware.py
@@ -3,20 +3,21 @@ import requests
 from dotenv import load_dotenv
 import os
 
+VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
 
 class IsTokenValidMiddleware(object):
 
     def __init__(self, get_response):
         self.get_response = get_response
+        self.url = 'api/token/verify/'
+        load_dotenv()
 
     def __call__(self, response):
         response = self.get_response(response)
         return response
 
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        load_dotenv()
-        VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
-        req = requests.post(VERIFY_TOKEN + 'api/token/verify/',
+        req = requests.post(VERIFY_TOKEN + self.url,
                             data={'token': request.META['HTTP_AUTHORIZATION'][3:]})
         if req.status_code == 401:
             return HttpResponseForbidden()
